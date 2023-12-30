60bb8de667552020b0a7b5d1531c0ba3c878f86a
HarukaDoo <buriakkdiana@gmail.com>
2023-12-30 15:59:45 +0100

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,38 @@
-import React from 'react';
-import logo from './logo.svg';
+/* eslint-disable jsx-a11y/anchor-is-valid */
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='container'>
+      <div className='inner__container'>
+        <div className='content__container'>
+          <div className="form">
+            <div className="form__container">
+              <div className="form__title">WELCOME!</div>
+              <div className="form__caption">
+                *you must to create an account first to enjoy all the features of the site
+              </div>
+
+              <form className='sign-up__form' action="#" method="post">
+                <input type="text" id='nickname' placeholder='your nickname' />
+
+                <input type="email" id='email' placeholder='your email' />
+
+                <input type="password" id='password' placeholder='your password' />
+
+                <div className="form__navigation">
+                  <a href="https://youtu.be/pyfjoqQzO5k?si=eyZPvCOllXNtK5cL" className="sign-in__button">sign in</a>
+                  <button type="submit" className="sign-up__button">sign up</button>
+                </div>
+              </form>
+            </div>
+          </div>
+        </div>
+
+        <div className='background'>
+          <div className='background__container'></div>
+        </div>
+      </div>
     </div>
   );
 }
